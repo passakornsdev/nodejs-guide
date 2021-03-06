058dae6fd30786a01d36bd8a144644f8656147ac
Jo <passakorn2110@gmail.com>
2021-03-06 16:39:30 +0700

    install body-parser and hook body-parser to middleware

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 //create express app
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware');
-    next(); // continue to next middleware
+app.use(bodyParser.urlencoded());
+
+app.get('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>');
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use((req, res, next) => {
-    console.log('In the another middleware');
     res.send('<h1>hello from express! </h1>');
 });
 
