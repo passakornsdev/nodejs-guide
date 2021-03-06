b71b959ba7184c0796bddc62beeafde917835f28
Jo <passakorn2110@gmail.com>
2021-03-06 16:20:17 +0700

    use express.js listen instead of http.listen

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const http = require('http');
-
 const express = require('express');
 
 //create express app
@@ -15,6 +13,4 @@ app.use((req, res, next) => {
     res.send('<h1>hello from express! </h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
