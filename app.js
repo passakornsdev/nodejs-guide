7b7ebf3132d3b31e1b3fe283d737bfed133a892e
Jo <passakorn2110@gmail.com>
2021-03-06 16:09:14 +0700

    install express.js and create simple express js app

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const http = require('http');
-const routes = require('./routes');
 
-// const server = http.createServer(routes);
-const server = http.createServer(routes.handler);
+const express = require('express');
+
+//create express app
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('In the middleware');
+    next(); // continue to next middleware
+});
+
+app.use((req, res, next) => {
+    console.log('In the another middleware');
+    res.send('<h1>hello from express! </h1>');
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
