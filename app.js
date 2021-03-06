8f79f17a6281a571a6597753543924a96828a8f0
Jo <passakorn2110@gmail.com>
2021-03-06 16:52:34 +0700

    use express route to manage contributed application routes

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const adminRoute = require('./routes/admin');
+const shopRoute = require('./routes/shop');
+
 //create express app
 const app = express();
 
 app.use(bodyParser.urlencoded());
 
-app.get('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>');
-});
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use((req, res, next) => {
-    res.send('<h1>hello from express! </h1>');
-});
+app.use(adminRoute);
+app.use(shopRoute);
 
 app.listen(3000);
