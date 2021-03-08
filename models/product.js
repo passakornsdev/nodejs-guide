c2f37cc7b3ed76676feaa4cb2eb08fe2b33ddf56
Jo <passakorn2110@gmail.com>
2021-03-09 00:46:51 +0700

    add admin pages, user shopping pages

--- models/product.js
+++ models/product.js
@@ -1,34 +1,40 @@
 const fs = require('fs');
 const path = require('path');
 
-const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
-const getProductsFromFile = (cb) => {
-    fs.readFile(p, (err, data) => { // use arrow function to reference to class
-        if (err) {
-            cb([]);
-        } else {
-            cb(JSON.parse(data));
-        }
-    });
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
 };
 
-module.exports = class product {
-    constructor(title) {
-        this.title = title;
-    }
+module.exports = class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
 
-    save() {
-        getProductsFromFile(products => {
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
 
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err) => {
-                console.log(err);
-            });
-        });
-    }
-
-    // call func via class not instantiate object
-    static fetchAll(callback) {
-        getProductsFromFile(callback);
-    }
-}
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
