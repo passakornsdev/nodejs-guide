913b6d23940b41caf74492d6e6a9a397e6b7249c
Jo <passakorn2110@gmail.com>
2021-03-10 23:27:43 +0700

    add relation between user and product, and add dummy user to application

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const adminRoutes = require('./routes/admin');
 const shopRoute = require('./routes/shop');
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 //create express app
 const app = express();
@@ -25,8 +27,29 @@ app.use(shopRoute);
 
 app.use(errorController.get404);
 
-sequelize.sync().then(() => {
-    app.listen(3000);
-}).catch(err => {
+Product.belongsTo(User, {
+    constraints: true,
+    onDelete: 'CASCADE'
+});
+User.hasMany(Product);
+
+sequelize
+    // .sync({force: true}) // not for prod
+    .sync() // not for prod
+    .then(() => {
+        return User.findByPk(1);
+    })
+    .then(user => {
+        if(!user) {
+            return User.create({name: 'Admin', email: 'admin@mail.com'})
+        }
+        // return Promise.resolve(user);
+        return user; // if return object in then block, value is converted to promise, just make sure value are equal
+    })
+    .then(user => {
+        console.log(user);
+        app.listen(3000);
+    })
+    .catch(err => {
     console.log(err);
 });
