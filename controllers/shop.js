a26deccbefbe89dc025ff8b1231383fe0da61b52
Jo <passakorn2110@gmail.com>
2021-03-11 00:08:53 +0700

    use delete product in cartItem table

--- controllers/shop.js
+++ controllers/shop.js
@@ -98,14 +98,21 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
     const productId = req.body.productId;
-    Product.findById(productId)
-        .then(([product]) => {
-            Cart.deleteProduct(productId, product[0].price);
+    req.user
+        .getCart()
+        .then(cart => {
+            return cart.getProducts({where: {id: productId}});
+        })
+        .then(products => {
+            const product = products[0];
+            return product.cartItem.destroy();
+        })
+        .then(() => {
             res.redirect('/cart');
         })
         .catch(err => {
             console.log(err);
-        });
+        })
 };
 
 exports.getOrders = (req, res, next) => {
