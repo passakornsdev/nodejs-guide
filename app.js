71b47c5c6ce4b92e0faad34d4a08d5ce3448e16d
Jo <passakorn2110@gmail.com>
2021-03-11 00:13:19 +0700

    create order, orderItem and add relation to user, product

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 //create express app
 const app = express();
@@ -53,10 +55,15 @@ Cart.belongsToMany(Product, {
 Product.belongsToMany(Cart, {
     through: CartItem
 });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, {
+    through: OrderItem
+})
 
 sequelize
     // .sync({force: true}) // not for prod
-    .sync() // not for prod
+    .sync()
     .then(() => {
         return User.findByPk(1);
     })
