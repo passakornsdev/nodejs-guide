f19337473961ca88eda4db53d23657ac70b91d4a
Jo <passakorn2110@gmail.com>
2021-03-12 00:37:26 +0700

    replace id with _id, create findById

--- models/product.js
+++ models/product.js
@@ -1,3 +1,5 @@
+const mongodb = require('mongodb');
+
 const getDb = require('../util/database').getDb;
 
 class Product {
@@ -28,6 +30,21 @@ class Product {
             .then(products => {
                 console.log(products);
                 return products;
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+    static findById(prodId) {
+        const db = getDb();
+        return db
+            .collection('products')
+            .find({_id: new mongodb.ObjectID(prodId)}) // mongodb generate id with ObjectId type
+            .next() // get next item
+            .then(product => {
+                console.log(product);
+                return product;
             })
             .catch(err => {
                 console.log(err);
