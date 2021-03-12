0154b1ff1556e9c2c18e514a68b8a95086a9fe4d
Jo <passakorn2110@gmail.com>
2021-03-12 15:55:06 +0700

    set user model object to req.user

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
     User
         .findById('000000013a5cd00d306a48dd')
         .then(user => {
-            req.user = user;
+            req.user = new User(user.name, user.email, user.cart, user._id);
             next();
         })
         .catch(err => {
