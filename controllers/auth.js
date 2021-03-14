d4f6b8a1776132af500bd10144315dfedbb3351e
passakornsdev <passakornsdev@gmail.com>
2021-03-14 15:58:23 +0700

    ensure session is saved before redirect

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,7 +13,12 @@ exports.postLogin = (req, res, next) => {
         .then(user => {
             req.session.isLoggedIn = true;
             req.session.user = user;
-            res.redirect('/');
+            req.session.save((err) => {
+                if(err) {
+                    console.log(err);
+                }
+                res.redirect('/');
+            });
         })
         .catch(err => console.log(err));
 }
