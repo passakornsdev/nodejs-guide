1cdc4c5ea60a5fd7a3e7191b85657999cb2edee8
passakornsdev <passakornsdev@gmail.com>
2021-03-14 18:02:23 +0700

    add sign up page

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,30 +1,58 @@
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
-    res.render('auth/login', {
-        path: '/login',
-        pageTitle: 'Login',
-        isAuthenticated: req.session.isLoggedIn
-    });
-}
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
+  });
+};
 
 exports.postLogin = (req, res, next) => {
-    User.findById('604bbad1f92bcd2720192577')
-        .then(user => {
-            req.session.isLoggedIn = true;
-            req.session.user = user;
-            req.session.save((err) => {
-                if(err) {
-                    console.log(err);
-                }
-                res.redirect('/');
-            });
-        })
-        .catch(err => console.log(err));
-}
+  User.findById('604bbad1f92bcd2720192577')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save(err => {
+        console.log(err);
+        res.redirect('/');
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  User.findOne({email})
+      .then(userDoc => {
+        if(userDoc) {
+          // TO DO ADD ERROR MSG
+          return res.redirect('/signup');
+        }
+        return new User({email, password, cart: {items: []}})
+            .save()
+      })
+      .then(() => {
+        res.redirect('/login')
+      })
+      .catch(err => {
+        console.log(err);
+      })
+};
 
 exports.postLogout = (req, res, next) => {
-    req.session.destroy(() => {
-        res.redirect('/');
-    });
-}
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
+};
